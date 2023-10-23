c72f0647610c6653e4b4e9bbbbc8b2804e5db111
August@Inflectra <92896669+Zie62@users.noreply.github.com>
2023-10-23 11:08:49 -0400

    Get it minifying a complex react library component correctly

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,29 @@
 const webpack = require("webpack");
 const TerserPlugin = require('terser-webpack-plugin');
+const Path = require('path');
 
 module.exports = {
     //Path of the production file from your target library
-    entry: "C:/git/Webpack Minification/node_modules/react-select/dist/react-select.cjs.prod.js",
+    entry: "C:/git/Webpack Minification/node_modules/react-select/creatable/dist/react-select-creatable.cjs.prod.js",
     devtool: 'source-map',
     output: {
         //Path for where you intend to put the minified files
         path: "C:/git/Webpack Minification/mini",
         filename: "react-select.min.js"
     },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({parallel: true})
+    module: {
+        rules: [
+            {
+                test: /\.(t|j)sx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
         ]
+    },
+    externals: {
+        react: "React"
     },
     mode: "production"
 }
